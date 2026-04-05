f90697cd6587195000851b84530685879f0253bc
TanKangZheng <tankangzheng@hotmail.com>
2026-04-06 06:36:29 +0800

    first commit

---
+++ src/GAthon/main.py
@@ -0,0 +1,10 @@
+import requests
+
+
+# response = requests.get("https://api.gatcg.com/cards/search?name=kongming&element=TERA")
+# if response.status_code == 200:
+#     # 3. Parse JSON data
+#     data = response.json()
+#     print(data)
+# else:
+#     print(f"Error: {response.status_code}")
