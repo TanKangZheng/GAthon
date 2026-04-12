fc8ae3704b1560affe4c6f400e5637742c20d3a7
TanKangZheng <tankangzheng@hotmail.com>
2026-04-12 19:53:21 +0800

    - Exposed the other classes info in GACardData.py

--- src/GAthon/__init__.py
+++ src/GAthon/__init__.py
@@ -1,3 +1,3 @@
 from .GAQuery import Search, QuickSearch, set_client, GAQuery
-from .GACardData import GACardData
+from .GACardData import GACardData, GACardEdition, Reference, SetInfo, CirculationTemplate
 from .GAEnums import Elements, Classes, Types, Rarity, Subtypes, LogicOperator, LogicParameter, Speed, Configuration, Language, Format, Legality
